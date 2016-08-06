f1d80263476642f6b69f420df684992a339019bf
Bruce <maljean2012@gmail.com>
2016-08-05 21:34:04 -0400

    resubmit

---
+++ ContosoUniversity/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class ProductController : Controller {
+        public ActionResult Index() {
+            // Add action logic here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Details(int Id) {
+
+            return View("Details");
+        }
+    }
+}
